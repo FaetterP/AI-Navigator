0d4d45a9beb7aaf5cd14f7d8f4d83cae9cb7c20a
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-03-30 01:05:51 +0300

    Добавлен класс для отрисовки всех клеточек

---
+++ Assets/Navigator/Draftsman.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Navigator
+{
+    class Draftsman : MonoBehaviour
+    {
+        [SerializeField] private int _x, _y;
+        [SerializeField] private GameObject _element;
+
+        private void Start()
+        {
+            Draw();
+        }
+
+        private void Draw()
+        {
+            for (int i = 0; i < _x; i++)
+            {
+                for (int j = 0; j < _y; j++)
+                {
+                    GameObject element = Instantiate(_element, transform);
+                    element.transform.localPosition = new Vector2(100 * (i - _x / 2f), 100 * (j - _y / 2f));
+                }
+            }
+        }
+    }
+}
