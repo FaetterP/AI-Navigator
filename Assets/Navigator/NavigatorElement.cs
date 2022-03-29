f19e44dcc6298e3f8332b65c87cea1adbd70fbe3
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-03-30 00:52:31 +0300

    Добавлен NavigatorElement
    Этот компонент отвечает за то, по какому маршруту нужно ехать на данном участке.

---
+++ Assets/Navigator/NavigatorElement.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Utilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Navigator
+{
+    [RequireComponent(typeof(Image))]
+    class NavigatorElement : MonoBehaviour
+    {
+        [SerializeField] private Sprite _forward;
+        [SerializeField] private Sprite _right;
+        [SerializeField] private Sprite _left;
+
+        private CycleArray<Sprite> _array;
+        private Image _thisImage;
+
+        private void Awake()
+        {
+            _thisImage = GetComponent<Image>();
+            _array = new CycleArray<Sprite>(new Sprite[] { _forward, _right, _left });
+        }
+
+        private void Start()
+        {
+            _thisImage.sprite = _array.GetNext();
+        }
+
+        private void OnMouseDown()
+        {
+            _thisImage.sprite = _array.GetNext();
+        }
+    }
+}
---
+++ Assets/Utilities/CycleArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assets.Utilities
+{
+    class CycleArray<T>
+    {
+        private T[] _array;
+        private int _index;
+
+
+        public CycleArray(T[] array)
+        {
+            _array = new T[array.Length];
+            Array.Copy(array, _array, array.Length);
+            _index = 0;
+        }
+
+        public T GetNext()
+        {
+            T ret = _array[_index];
+            _index = (_index + 1) % _array.Length;
+            return ret;
+        }
+
+    }
+}
